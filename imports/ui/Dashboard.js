b9932c4ee18dac7e357ee0810cc7a8263f53e698
Jay Allen <allen.jeremiah555@gmail.com>
2018-01-24 01:17:17 -0500

    Initial Commit

---
+++ imports/ui/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import PrivateHeader from './PrivateHeader';
+
+export default class Dashboard extends React.Component {
+
+  componentWillMount() {
+    if (!Meteor.userId()) {
+      this.props.history.replace('/')
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <PrivateHeader title={'Dashboard'} />
+        <div className='page-content'>
+          Dashboard page content
+        </div>
+      </div>
+    )
+  }
+}
